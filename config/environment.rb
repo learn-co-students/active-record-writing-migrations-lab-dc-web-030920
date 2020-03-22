1c727bdd002d3b9e068c661137411014a350c51d
itacamba <itacamba@live.com>
2020-03-22 16:47:30 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -18,3 +18,10 @@ def drop_db
     DB.execute("DROP TABLE #{table}")
   end
 end
+
+# # CONNECTION TO DB
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => db/migrate/school-development
+
+# # )
